e0c8fc03bc3d56998e17b68e7b7ecce66087adc3
Dimitris Kokkonis <kokkonisd@gmail.com>
2021-01-09 19:43:01 +0200

    Add tests for add command

--- paignion/tests/test_action_compiler.py
+++ paignion/tests/test_action_compiler.py
@@ -116,3 +116,134 @@ class TestActionCompiler(unittest.TestCase):
             str(err.exception),
             "Expected token type `comma` but got `md_string`",
         )
+
+    def test_normal_add_command(self):
+        ac = ActionCompiler()
+
+        res = ac.compile_action('add("The door is now unlocked.", description, origin)')
+        self.assertEqual(
+            res,
+            'getRoomOrItem("origin")["description"] += "The door is now unlocked.";',
+        )
+
+        # Whitespace should not matter
+        res = ac.compile_action(
+            'add (  "The door is now unlocked.",description ,  \t\t     origin)'
+        )
+        self.assertEqual(
+            res,
+            'getRoomOrItem("origin")["description"] += "The door is now unlocked.";',
+        )
+
+        # Quotes should not matter for keys and objects (amount and book case in this
+        # case)
+        res = ac.compile_action('add(" It also smells bad.", "description", "sock")')
+        self.assertEqual(
+            res, 'getRoomOrItem("sock")["description"] += " It also smells bad.";'
+        )
+
+        res = ac.compile_action('add(241, amount, "book case")')
+        self.assertEqual(res, 'getRoomOrItem("book case")["amount"] += 241;')
+
+        # Markdown strings should render correctly
+        res = ac.compile_action(
+            'add(m" Very, _very_ **broken**.", description, "old door")'
+        )
+        self.assertEqual(
+            res,
+            'getRoomOrItem("old door")["description"] += "<p>Very, <em>very</em> '
+            '<strong>broken</strong>.</p>";',
+        )
+
+    def test_failed_add_command(self):
+        ac = ActionCompiler()
+
+        # Test typo in add keyword
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            res = ac.compile_action('ad(241, amount, "book case")')
+
+        self.assertEqual(
+            str(err.exception),
+            "Undefined structure starting with `IDENTIFIER_TOKEN(`ad`)` for action "
+            '`ad(241, amount, "book case")`',
+        )
+
+        # Test missing one argument
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action('add("west", "description")')
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `comma` but got `cparen`"
+        )
+
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action('add("west", "description",)')
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `identifier` but got `cparen`"
+        )
+
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action('add("west", ,origin)')
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `identifier` but got `comma`"
+        )
+
+        # Test missing two arguments
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action('add("west")')
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `comma` but got `cparen`"
+        )
+
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action('add("west",)')
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `identifier` but got `cparen`"
+        )
+
+        # Test missing all arguments
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("add(   )")
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `string` but got `cparen`"
+        )
+
+        # Test missing parentheses
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action('add m" _west_" to "description" for origin')
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `oparen` but got `md_string`"
+        )
+
+        # Test missing closing parenthesis
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action('add("west", description, origin')
+
+        self.assertEqual(
+            str(err.exception),
+            "Expected token type `cparen` but there are no more tokens",
+        )
+
+        # Test no commas
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action('add(m"This is now the _hidden_ room" description origin')
+
+        self.assertEqual(
+            str(err.exception),
+            "Expected token type `comma` but got `identifier`",
+        )
+
+        # Test missing quotes for string
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("add(This is now the hidden room, description, origin")
+
+        self.assertEqual(
+            str(err.exception),
+            "Expected token type `string` but got `identifier`",
+        )
