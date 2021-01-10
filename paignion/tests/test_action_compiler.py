2d903e2d4a2288439e92e48f187370cc93e1c204
Dimitris Kokkonis <kokkonisd@gmail.com>
2021-01-10 13:43:57 +0200

    Complete action compiler tests

--- paignion/tests/test_action_compiler.py
+++ paignion/tests/test_action_compiler.py
@@ -197,6 +197,10 @@ class TestActionCompiler(unittest.TestCase):
             '<strong>broken</strong>.</p>";',
         )
 
+        # Negative integers should also work
+        res = ac.compile_action("add(-10, amount, coin)")
+        self.assertEqual(res, 'getRoomOrItem("coin")["amount"] += -10;')
+
     def test_failed_add_command(self):
         ac = ActionCompiler()
 
@@ -291,19 +295,339 @@ class TestActionCompiler(unittest.TestCase):
         )
 
     def test_normal_sub_command(self):
-        pass  # TODO
+        ac = ActionCompiler()
+
+        res = ac.compile_action('sub(10, amount, "health points")')
+        self.assertEqual(res, 'getRoomOrItem("health points")["amount"] -= 10;')
+
+        # Whitespace should not matter
+        res = ac.compile_action('sub (  10,amount ,  \t\t     "health points")')
+        self.assertEqual(res, 'getRoomOrItem("health points")["amount"] -= 10;')
+
+        # Negative numbers should pass
+        res = ac.compile_action('sub(-2, amount, "health points")')
+        self.assertEqual(res, 'getRoomOrItem("health points")["amount"] -= -2;')
+
+        # Zero should also pass
+        res = ac.compile_action('sub(-0, amount, "health points")')
+        self.assertEqual(res, 'getRoomOrItem("health points")["amount"] -= 0;')
 
     def test_failed_sub_command(self):
-        pass  # TODO
+        ac = ActionCompiler()
+
+        # Test typo in sub keyword
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action('sb(10, amount, "health points")')
+
+        self.assertEqual(
+            str(err.exception),
+            "Undefined structure starting with `IDENTIFIER_TOKEN(`sb`)` for action "
+            '`sb(10, amount, "health points")`',
+        )
+
+        # Test missing one argument
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("sub(10, amount)")
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `comma` but got `cparen`"
+        )
+
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("sub(10, amount,)")
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `identifier` but got `cparen`"
+        )
+
+        # Test missing two arguments
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("sub(10)")
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `comma` but got `cparen`"
+        )
+
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("sub(10, )")
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `identifier` but got `cparen`"
+        )
+
+        # Test missing all arguments
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("sub(  )")
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `integer` but got `cparen`"
+        )
+
+        # Test missing parentheses
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("sub 10 from amount for coin")
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `oparen` but got `integer`"
+        )
+
+        # Test missing closing parenthesis
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("sub(10, amount, coin")
+
+        self.assertEqual(
+            str(err.exception),
+            "Expected token type `cparen` but there are no more tokens",
+        )
+
+        # Test passing Markdown string or normal string instead of integer
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action('sub("aaa", description, origin)')
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `integer` but got `string`"
+        )
+
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action('sub(m"_aaa_", description, origin)')
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `integer` but got `md_string`"
+        )
+
+        # Test passing invalid integer
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("sub(10f, description, origin)")
+
+        self.assertEqual(
+            str(err.exception),
+            "Undefined token `1` in `10f, description, origin)`, in action "
+            "`sub(10f, description, origin)`",
+        )
 
     def test_normal_mul_command(self):
-        pass  # TODO
+        ac = ActionCompiler()
+
+        res = ac.compile_action('mul(10, amount, "health points")')
+        self.assertEqual(res, 'getRoomOrItem("health points")["amount"] *= 10;')
+
+        # Whitespace should not matter
+        res = ac.compile_action('mul (  10,amount ,  \t\t     "health points")')
+        self.assertEqual(res, 'getRoomOrItem("health points")["amount"] *= 10;')
+
+        # Negative numbers should pass
+        res = ac.compile_action('mul(-2, amount, "health points")')
+        self.assertEqual(res, 'getRoomOrItem("health points")["amount"] *= -2;')
+
+        # Zero should also pass
+        res = ac.compile_action('mul(-0, amount, "health points")')
+        self.assertEqual(res, 'getRoomOrItem("health points")["amount"] *= 0;')
 
     def test_failed_mul_command(self):
-        pass  # TODO
+        ac = ActionCompiler()
+
+        # Test typo in sub keyword
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action('ml(10, amount, "health points")')
+
+        self.assertEqual(
+            str(err.exception),
+            "Undefined structure starting with `IDENTIFIER_TOKEN(`ml`)` for action "
+            '`ml(10, amount, "health points")`',
+        )
+
+        # Test missing one argument
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("mul(10, amount)")
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `comma` but got `cparen`"
+        )
+
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("mul(10, amount,)")
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `identifier` but got `cparen`"
+        )
+
+        # Test missing two arguments
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("mul(10)")
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `comma` but got `cparen`"
+        )
+
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("mul(10, )")
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `identifier` but got `cparen`"
+        )
+
+        # Test missing all arguments
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("mul(  )")
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `integer` but got `cparen`"
+        )
+
+        # Test missing parentheses
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("mul 10 from amount for coin")
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `oparen` but got `integer`"
+        )
+
+        # Test missing closing parenthesis
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("mul(10, amount, coin")
+
+        self.assertEqual(
+            str(err.exception),
+            "Expected token type `cparen` but there are no more tokens",
+        )
+
+        # Test passing Markdown string or normal string instead of integer
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action('mul("aaa", description, origin)')
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `integer` but got `string`"
+        )
+
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action('mul(m"_aaa_", description, origin)')
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `integer` but got `md_string`"
+        )
+
+        # Test passing invalid integer
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("mul(10f, description, origin)")
+
+        self.assertEqual(
+            str(err.exception),
+            "Undefined token `1` in `10f, description, origin)`, in action "
+            "`mul(10f, description, origin)`",
+        )
 
     def test_normal_div_command(self):
-        pass  # TODO
+        ac = ActionCompiler()
+
+        res = ac.compile_action('div(10, amount, "health points")')
+        self.assertEqual(res, 'getRoomOrItem("health points")["amount"] /= 10;')
+
+        # Whitespace should not matter
+        res = ac.compile_action('div (  10,amount ,  \t\t     "health points")')
+        self.assertEqual(res, 'getRoomOrItem("health points")["amount"] /= 10;')
+
+        # Negative numbers should pass
+        res = ac.compile_action('div(-2, amount, "health points")')
+        self.assertEqual(res, 'getRoomOrItem("health points")["amount"] /= -2;')
 
     def test_failed_div_command(self):
-        pass  # TODO
+        ac = ActionCompiler()
+
+        # Test division by zero
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action('div(0, amount, "health points")')
+
+        self.assertEqual(
+            str(err.exception),
+            "div() action with 0 detected, cannot divide by 0",
+        )
+
+        # Test typo in sub keyword
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action('dv(10, amount, "health points")')
+
+        self.assertEqual(
+            str(err.exception),
+            "Undefined structure starting with `IDENTIFIER_TOKEN(`dv`)` for action "
+            '`dv(10, amount, "health points")`',
+        )
+
+        # Test missing one argument
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("div(10, amount)")
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `comma` but got `cparen`"
+        )
+
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("div(10, amount,)")
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `identifier` but got `cparen`"
+        )
+
+        # Test missing two arguments
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("div(10)")
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `comma` but got `cparen`"
+        )
+
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("div(10, )")
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `identifier` but got `cparen`"
+        )
+
+        # Test missing all arguments
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("div(  )")
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `integer` but got `cparen`"
+        )
+
+        # Test missing parentheses
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("div 10 from amount for coin")
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `oparen` but got `integer`"
+        )
+
+        # Test missing closing parenthesis
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("div(10, amount, coin")
+
+        self.assertEqual(
+            str(err.exception),
+            "Expected token type `cparen` but there are no more tokens",
+        )
+
+        # Test passing Markdown string or normal string instead of integer
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action('div("aaa", description, origin)')
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `integer` but got `string`"
+        )
+
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action('div(m"_aaa_", description, origin)')
+
+        self.assertEqual(
+            str(err.exception), "Expected token type `integer` but got `md_string`"
+        )
+
+        # Test passing invalid integer
+        with self.assertRaises(PaignionActionCompilerException) as err:
+            ac.compile_action("div(10f, description, origin)")
+
+        self.assertEqual(
+            str(err.exception),
+            "Undefined token `1` in `10f, description, origin)`, in action "
+            "`div(10f, description, origin)`",
+        )
