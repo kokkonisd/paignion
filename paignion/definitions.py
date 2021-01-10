651efedbd7b109aa10567a6b7d2249cea2f82695
Dimitris Kokkonis <kokkonisd@gmail.com>
2021-01-11 01:19:55 +0200

    Split version into __version__ and __version_info__

--- paignion/definitions.py
+++ paignion/definitions.py
@@ -2,7 +2,8 @@ import os
 
 
 # The current version of Paignion
-__version__ = "0.0.6"
+__version_info__ = ("0", "0", "6")
+__version__ = ".".join(__version_info__)
 
 # The base directory of the project
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
